d0297fb0d99e8faf0a85bda4e77a6c0ab32c6b9e
Aleksandr Poliakov <aleksandr.poliakov@clearmatics.com>
2019-04-03 13:42:04 +0100

    test write to secret

--- cm_writer/main.py
+++ cm_writer/main.py
@@ -2,6 +2,20 @@
 
 from kubernetes import client, config
 
+def write_to_cm_pass():
+    api_instance = client.CoreV1Api()
+
+    sec = client.V1Secret()
+    sec.type = "Opaque"
+    sec.data = {"username": "TestUser", "password": "TestPass"}
+    name = 'account-pwd'
+    namespace = 'testnewautonity'
+    body = sec
+    try:
+        api_response = api_instance.patch_namespaced_secret(name, namespace, body)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when write to secret: %s\n" % e)
 
 def main():
 
