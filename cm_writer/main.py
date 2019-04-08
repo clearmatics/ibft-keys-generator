8e6cfbd1b3540b2edf6521a480baf37a59a9a82f
Aleksandr Poliakov <aleksandr.poliakov@clearmatics.com>
2019-04-08 15:28:28 +0100

    Generate password to k8s secret

--- cm_writer/main.py
+++ cm_writer/main.py
@@ -9,19 +9,18 @@ from kubernetes import client, config
 from pprint import pprint
 
 
-def write_to_cm_pass():
+def write_account_pass(namespace):
+    # Generate password for account and write it to k8s secret
+
     api_instance = client.CoreV1Api()
 
     alphabet = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(secrets.choice(alphabet) for i in range(30))  # for a 30-character password
-    print('Password:' + password)
-    print('Base64:' + base64.b64encode(password.encode()).decode())
 
     sec = client.V1Secret()
     sec.type = 'Opaque'
     sec.data = {'account.pwd': base64.b64encode(password.encode()).decode()}
     name = 'account-pwd'
-    namespace = 'testnewautonity'
     body = sec
     try:
         api_response = api_instance.patch_namespaced_secret(name, namespace, body)
@@ -32,34 +31,30 @@ def write_to_cm_pass():
 
 def main():
     parser = argparse.ArgumentParser(description='Generate set of keys for initialisation networks and deploy it to kubernetes secrets')
-    parser.add_argument('-k','--kube-config',
+    parser.add_argument('-k',
                         dest='kubeconf_type',
                         default='pod',
                         choices=['pod', 'remote'],
                         help='Type of connection to kube-apiserver: pod or remote (default: %(default)s)'
+                        )
+    parser.add_argument('-n',
+                        dest='namespace',
+                        default='default',
+                        help='Kubernetes namespace.'
                         )
     args = parser.parse_args()
     pprint(vars(args))
 
     if args.kubeconf_type == 'pod':
         config.load_incluster_config()
+        f = open("/var/run/secrets/kubernetes.io/serviceaccount/namespace", "r")
+        namespace = f.readline()
+        f.close()
     elif args.kubeconf_type == 'remote':
         config.load_kube_config()
+        namespace = args.namespace
 
-
-    write_to_cm_pass()
+    write_account_pass(namespace)
 
 if __name__ == '__main__':
     main()
-
-
-
-kubectl get pods -o json | jq
-
-
-# Get ExternalIPs of all nodes
-kubectl get nodes -o jsonpath='{.items[*].status.addresses[?(@.type=="ExternalIP")].address}'\
-
-                              
-
-                              '.items[].spec.containers[].env[]?.valueFrom.secretKeyRef.name'
