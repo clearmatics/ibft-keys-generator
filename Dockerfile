e04e7b4e8f5b59e7d06fb71076cc5482ee12b7dd
Aleksandr Poliakov <aleksandr.poliakov@clearmatics.com>
2019-04-09 18:57:20 +0100

    Generate private keys for validators and observers. Write it to k8s secrets

--- Dockerfile
+++ Dockerfile
@@ -21,9 +21,13 @@ FROM python:3.7.3-alpine
 #
 #RUN npm install
 
+
+RUN apk add --no-cache --virtual .build-deps gcc musl-dev
+
 WORKDIR /env/cm_writer
 ADD ./cm_writer/requirements.txt .
 RUN pip install -r ./requirements.txt
+RUN apk del .build-deps gcc musl-dev
 #ADD ./cm_writer/requirements.txt .
 #RUN virtualenv ./ && source ./bin/activate && pip install -r ./requirements.txt
 COPY ./cm_writer /cm_writer
--- cm_writer/main.py
+++ cm_writer/main.py
@@ -4,9 +4,11 @@ import base64
 import argparse
 import secrets
 import string
+import sha3
 from kubernetes.client.rest import ApiException
 from kubernetes import client, config
 from pprint import pprint
+from ecdsa import SigningKey, SECP256k1
 
 
 def write_account_pass(namespace):
@@ -29,6 +31,46 @@ def write_account_pass(namespace):
         print("Exception when write to secret: %s\n" % e)
 
 
+def checksum_encode(addr_str): # Takes a hex (string) address as input
+    keccak = sha3.keccak_256()
+    out = ''
+    addr = addr_str.lower().replace('0x', '')
+    keccak.update(addr.encode('ascii'))
+    hash_addr = keccak.hexdigest()
+    for i, c in enumerate(addr):
+        if int(hash_addr[i], 16) >= 8:
+            out += c.upper()
+        else:
+            out += c
+    return '0x' + out
+
+
+def generate_keys():
+    keccak = sha3.keccak_256()
+
+    priv = SigningKey.generate(curve=SECP256k1)
+    pub = priv.get_verifying_key().to_string()
+
+    keccak.update(pub)
+    address = keccak.hexdigest()[24:]
+    return {'private_key': priv.to_string().hex(), 'pub_key': pub.hex(), 'address': checksum_encode(address)}
+
+
+def write_keys(peers, kind, namespace):
+    api_instance = client.CoreV1Api()
+    sec = client.V1Secret()
+    sec.type = 'Opaque'
+    payload = {}
+    for peer in peers:
+        payload[peer['id']] = base64.b64encode(peer['private_key'].encode()).decode()
+    sec.data = payload
+    try:
+        api_response = api_instance.patch_namespaced_secret(kind, namespace, sec)
+        pprint(api_response)
+    except ApiException as e:
+        print("Exception when write to secret: %s\n" % e)
+
+
 def main():
     parser = argparse.ArgumentParser(description='Generate set of keys for initialisation networks and deploy it to kubernetes secrets')
     parser.add_argument('-k',
@@ -41,6 +83,18 @@ def main():
                         dest='namespace',
                         default='default',
                         help='Kubernetes namespace.'
+                        )
+    parser.add_argument('--validators',
+                        dest='validators',
+                        default=4,
+                        type=int,
+                        help='number of validators. Should be more than 4 for ibft (default: %(default)s)'
+                        )
+    parser.add_argument('--observers',
+                        dest='observers',
+                        default=1,
+                        type=int,
+                        help='number of observers minimum 1 (default: %(default)s)'
                         )
     args = parser.parse_args()
     pprint(vars(args))
@@ -55,6 +109,23 @@ def main():
         namespace = args.namespace
 
     write_account_pass(namespace)
+
+    validators = []
+    for i in range(args.validators):
+        validator = generate_keys()
+        validator['id'] = i
+        validators.append(validator)
+        print(validator)
+    write_keys(validators, 'validators', namespace)
+
+    observers = []
+    for i in range(args.observers):
+        observer = generate_keys()
+        observer['id'] = i
+        observers.append(observer)
+        print(observer)
+    write_keys(observers, 'observers', namespace)
+
 
 if __name__ == '__main__':
     main()
